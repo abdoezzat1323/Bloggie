80ade8ef05a91f342e6c9264f4c8a0e932b374e6
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2022-04-20 20:33:12 +0200

    more add

--- frontend/src/topbar/TopBar.jsx
+++ frontend/src/topbar/TopBar.jsx
@@ -2,22 +2,24 @@ import "./topbar.css"
 
 export default function topBar() {
   return (
+
     <header>
-    <div className="left_section">
-      <i className="material-icons logo  ">menu</i>
-      {/* <img className="image1" src="../../img/logo.jpg"  /> */}
-      
-    </div>
-    <div className="center_section">
-    <label className="logolable" >Blog</label>
-    </div>
-  
-    <div className="right_section">
-      <a href><i className="material-icons  "  >notifications</i></a>
-      <a href><i className="material-icons">account_circle</i></a>
-      
-    </div>
-  </header>
+      <div className="left_section">
+        <i className="material-icons logo sizeleft ">menu</i>
+        {/* <img className="image1" src="../../img/logo.jpg"  /> */}
+        
+      </div>
+      <div className="center_section">
+      <label className="logolable" >Blog</label>
+      </div>
+    
+      <div className="right_section  sizeright ">
+        <a href><i className="material-icons">notifications</i></a>
+        <a href><i className="material-icons">account_circle</i></a>
+        
+      </div>
+    </header>
+
   )
 }
 
