3014ffd21f63a08815c982412a1ac066f3d86306
mostafa-21 <101065375+mostafa-21@users.noreply.github.com>
2022-04-22 05:11:16 +0200

    fixed some errors

--- frontend/src/component/editor/JoditEditor.jsx
+++ frontend/src/component/editor/JoditEditor.jsx
@@ -1,22 +1,22 @@
-import React, { useRef } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+// import React, { useRef } from 'react';
+// import { Editor } from '@tinymce/tinymce-react';
 
-const JoditEditor = () => {
-  const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
-  return (
-    <>
-      <Editor
-        onInit={(_evt , editor) => editorRef.current = editor}
+// const JoditEditor = () => {
+//   const editorRef = useRef(null);
+//   const log = () => {
+//     if (editorRef.current) {
+//       console.log(editorRef.current.getContent());
+//     }
+//   };
+//   return (
+//     <>
+//       <Editor
+//         onInit={(_evt , editor) => editorRef.current = editor}
 
-      />
-      <button onClick={log}>Log editor content</button>
-    </>
-  );
-}
+//       />
+//       <button onClick={log}>Log editor content</button>
+//     </>
+//   );
+// }
 
-export default JoditEditor;
+// export default JoditEditor;
