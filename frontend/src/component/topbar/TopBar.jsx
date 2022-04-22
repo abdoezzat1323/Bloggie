d90bd31b53b1618d6bd3bcd117b716d83b890779
m-agour <mo.aggour@gmail.com>
2022-04-22 22:56:32 +0200

    added some cons

--- frontend/src/component/topbar/TopBar.jsx
+++ frontend/src/component/topbar/TopBar.jsx
@@ -1,21 +1,26 @@
 import "./topbar.css";
 import { NotificationContainer } from "react-notifications";
 import "react-notifications/lib/notifications.css";
+import { FiLogOut } from "react-icons/fi";
+import { BiHomeHeart } from "react-icons/bi";
 
 export default function topBar() {
   return (
     <header>
       <NotificationContainer />
-      <div className="left_section">
-        <i className="material-icons logo sizeleft ">menu</i>
+      <div className="left_section sizeleft">
+        {/* <a className="headerLabel" href="/">
+          <BiHomeHeart className="headerIcon"></BiHomeHeart>
+        </a> */}
+        {/* <i className="material-icons logo sizeleft ">menu</i> */}
         {/* <img className="image1" src="../../img/logo.jpg"  /> */}
       </div>
       <div className="center_section">
         <label className="logolable">Blog</label>
       </div>
       <div className="right_section  sizeright ">
-        <a href="/">
-          <i className="material-icons">account_circle</i>
+        <a className="headerLabel" href="/">
+          <FiLogOut className="headerIcon"></FiLogOut>
         </a>
       </div>
     </header>
