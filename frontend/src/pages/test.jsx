88da8f3d53284d241d7adab585a26eb61a545d13
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2022-04-22 02:28:53 +0200

    add

--- frontend/src/pages/test.jsx
+++ frontend/src/pages/test.jsx
@@ -1,24 +1,31 @@
-// import React, { useRef } from 'react';
-// import { Editor } from '@tinymce/tinymce-react';
+// import React, { Component } from 'react';
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-// export default function App() {
-//   const editorRef = useRef(null);
-//   const log = () => {
-//     if (editorRef.current) {
-//       console.log(editorRef.current.getContent());
+// function Ass(){
+//         return (
+//             <div className="App">
+//                 <CKEditor
+//                     editor={ ClassicEditor }
+//                     data="<p>Hello from CKEditor 5!</p>"
+//                     onReady={ editor => {
+//                         // You can store the "editor" and use when it is needed.
+//                         console.log( 'Editor is ready to use!', editor );
+//                     } }
+//                     onChange={ ( event, editor ) => {
+//                         const data = editor.getData();
+//                         console.log( { event, editor, data } );
+//                     } }
+//                     onBlur={ ( event, editor ) => {
+//                         console.log( 'Blur.', editor );
+//                     } }
+//                     onFocus={ ( event, editor ) => {
+//                         console.log( 'Focus.', editor );
+//                     } }
+//                 />
+//             </div>
+//         );
 //     }
-//   };
-//   return (
-//     <>
-//     <Editor
-//          onInit={(evt, editor) => editorRef.current = editor}
-//          initialValue="<p>This is the initial content of the editor.</p>"
-
-//        />
-//     <script src="/path/to/tinymce.min.js"></script>
 
 
-//       <button onClick={log}>Log editor content</button>
-//     </>
-//   );
-// }
+// export default Ass;
