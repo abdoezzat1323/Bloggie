77396a3807515b4dd2006cae51fb60484929f345
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2022-04-23 06:52:41 +0200

    write page edit2

--- frontend/src/component/write_postSidebar/write_postSideBar.jsx
+++ frontend/src/component/write_postSidebar/write_postSideBar.jsx
@@ -1,44 +1,42 @@
 import "./write_postsidebar.css"
-import {ImStatsDots} from 'react-icons/im';
-import {FaPaintBrush,FaRegShareSquare} from 'react-icons/fa';
-import {BiPaintRoll,BiCommentDetail} from 'react-icons/bi';
-import {BsFiles} from 'react-icons/bs';
-import {MdSettings} from 'react-icons/md';
-import {FiLink} from 'react-icons/fi';
-
+import "./multiselect.css"
+import React, { useState } from "react";
+import { MultiSelect } from "@progress/kendo-react-dropdowns";
 
 
 
 export default function Write_PostSideBar() {
+    const sports = [
+        "Baseball",
+        "Basketball",
+        "Cricket",
+        "Field Hockey",
+        "Football",
+        "Table Tennis",
+        "Tennis",
+        "Volleyball",
+      ];
+
+    const [title, setTitle] = useState('')
+    
+
   return (
-    <div className="write_postsidebar">
-        
-        <ul>
-            <li className="WSideItem">
-                <FaRegShareSquare className="ADIcon"/>Shares
-            </li>
-            <li className="WSideItem">
-                <ImStatsDots className="WRIcon"/>Statistcs
-            </li>
-            <li className="WSideItem">
-                <BiCommentDetail className="WRIcon"/>Comments
-            </li>
-            <li className="WSideItem">
-                <BsFiles className="WRIcon"/>Pages
-            </li>
-            <li className="WSideItem">
-                <BiPaintRoll className="WRIcon"/>Format
-            </li>
-            <li className="WSideItem">
-                <FaPaintBrush className="WRIcon"/>Theme
-            </li>
-            <li className="WSideItem">
-                <MdSettings className="WRIcon"/>Settings
-            </li>            
-            <li className=" Goblog">
-                <FiLink className="WRIcon"/>Go To Blog
-            </li>            
-        </ul>
+      
+    <div className="write_postsidebar  ">
+        <button  className="input11"  type="button"  > Post </button>
+        <h1 className="WSideItem" />
+        <div className="lable1 ">
+          <label className="label2" >Chose youre categorys</label>
+            <MultiSelect
+            className="k-badge  webkit-input-placeholder"
+            onChange={e => setTitle(e.target.value)}
+            data={sports}
+            placeholder="click here to chose..(^^)"
+            />
+        </div>  
+            <button type="button" onClick={() => alert(title)} className="btn">Save</button>
+        <h1 className="WSideItem" />
+          
         
     </div>
   )
