8e71aebbabffa2d98d68896c653efe9a12b230ca
mostafa-21 <101065375+mostafa-21@users.noreply.github.com>
2022-04-24 21:53:18 +0200

    setting

--- frontend/src/component/admincom/settings/Settings.jsx
+++ frontend/src/component/admincom/settings/Settings.jsx
@@ -1,7 +1,38 @@
 import "./settings.css"
+import {FaUserCircle} from 'react-icons/fa';
+import {FaEdit} from 'react-icons/fa';
+import {MdDone,MdDeleteForever} from 'react-icons/md';
+import {AiFillDelete} from 'react-icons/ai';
 
 export default function Settings() {
   return (
-    <div>Settings</div>
+    <div className="settingswar">
+      <div className="profileedit">
+        <div className="mails">
+          <h2>Profile</h2>
+          <label htmlFor="">Email</label>
+          <input type="text" />
+          <MdDone className="verfiy"/>
+          <AiFillDelete className="deletemail"/>
+          <hr />
+          <label htmlFor="">Facebook</label>
+          <input type="text" />
+          <MdDone className="verfiy"/>
+          <AiFillDelete className="deletemail"/>
+          <hr/>
+          <label htmlFor="">Twitter</label>
+          <input type="text" />
+          <MdDone className="verfiy"/>
+          <AiFillDelete className="deletemail"/>
+        </div>
+        <div className="photo">
+          <div className="photoshow">
+            <FaUserCircle className="photoIcon"/>
+            <FaEdit className="editPhoto"/>
+          </div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
