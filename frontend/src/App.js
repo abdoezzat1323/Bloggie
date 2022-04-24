165ed1d77caf5046cd5c8d27e9bebc0fc89ac050
m-agour <mo.aggour@gmail.com>
2022-04-24 19:39:13 +0200

    some other additions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Register from "./pages/signup/signup";
 import Home from "./pages/home2/Home";
 
 import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
-import Write from "./pages/write_post/write_post";
+import Write from "./pages/write/Write";
 import Writen from "./pages/test";
 import Admin from "./pages/admin/Admin";
 import Grid from "./component/grid/Grid";
@@ -13,16 +13,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={user ? <Home /> : <Login />} />
-        <Route path="/Register" element={user ? <Home /> : <Register />} />
-        <Route path="/grid" element={<Grid />} />
-        <Route path="/write_post" element={<Write />} />
-        <Route path="/write" element={<Writen />} />
-        <Route path="/admin/*" element={<Admin />} />
-        {/* <Route path="/write" element={< JoditEditor />} /> */}
-        {/* <Route path="/test" element={< Ass />} /> */}
-      </Routes>
+        <Route path="/" element={<Home />} />{" "}
+        <Route path="/login" element={user ? <Home /> : <Login />} />{" "}
+        <Route path="/Register" element={user ? <Home /> : <Register />} />{" "}
+        <Route path="/grid" element={<Grid />} />{" "}
+        <Route path="/write_post" element={<Write />} />{" "}
+        <Route path="/write" element={<Writen />} />{" "}
+        <Route path="/admin/*" element={<Admin />} />{" "}
+        {/* <Route path="/write" element={< JoditEditor />} /> */}{" "}
+        {/* <Route path="/test" element={< Ass />} /> */}{" "}
+      </Routes>{" "}
     </BrowserRouter>
   );
 }
