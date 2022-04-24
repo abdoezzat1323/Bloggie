e9a1b5df2239150d29207a6a67807cdae6905e09
mostafa-21 <101065375+mostafa-21@users.noreply.github.com>
2022-04-24 12:47:53 +0200

    setting hello

--- frontend/src/component/admincom/settings/Settings.jsx
+++ frontend/src/component/admincom/settings/Settings.jsx
@@ -2,6 +2,19 @@ import "./settings.css"
 
 export default function Settings() {
   return (
-    <div>Settings</div>
+    <div className="settingswar">
+      <div className="profileedit">
+        <div className="mails">
+          <h2>Profile</h2>
+          <label htmlFor="">Email</label>
+          <input type="text" />
+          <label htmlFor="">Facebook</label>
+          <input type="text" />
+          <label htmlFor="">Twitter</label>
+          <input type="text" />
+        </div>
+        <div className="photo">photo</div>
+      </div>
+    </div>
   )
 }
