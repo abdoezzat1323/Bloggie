364e37ec21add4be53f92495201c1308301b06c4
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2022-05-18 04:19:42 +0200

    css_view_post

--- frontend/src/pages/view_post/view_post.jsx
+++ frontend/src/pages/view_post/view_post.jsx
@@ -5,6 +5,7 @@ import ViewPostSideBar from "../../component/viewpostsidbar/viewsidebar";
 
 
 
+
   
 export default function View_post(){
 
@@ -17,7 +18,7 @@ export default function View_post(){
     e.preventDefault();
     console.log(Comment)
 }
-let x =`<p style="text-align: center;"><span style="font-size: 36pt;">my name is abdelrhman eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeezzat that is my name fo r you</span></p>
+let x =`<p style="text-align: center;"><span style="font-size: 36pt;">my name is abdelrhman ezzat that is my name fo r you</span></p>
 <p style="text-align: center;"><span style="font-size: 36pt;"><img src="http://127.0.0.1:5000/uploads/file-1652824358819.jpeg" alt="" width="256" height="197"></span></p>`
   
 let y = `<p style="text-align: center;"><span style="font-size: 36pt;">my name is abdelrhman eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeezzat that is my name fo r you</span></p>
@@ -30,9 +31,8 @@ let y = `<p style="text-align: center;"><span style="font-size: 36pt;">my name i
       </div>
       <div className="writeWar">
        <ViewPostSideBar/>
-      <div className="writeontent">
-          <img src="https://images.ctfassets.net/hrltx12pl8hq/5596z2BCR9KmT1KeRBrOQa/4070fd4e2f1a13f71c2c46afeb18e41c/shutterstock_451077043-hero1.jpg" alt="" className="img" />
-          <h1></h1>
+        <div className="writeontent">
+          <div className="imgdiv"><img src="https://images.ctfassets.net/hrltx12pl8hq/5596z2BCR9KmT1KeRBrOQa/4070fd4e2f1a13f71c2c46afeb18e41c/shutterstock_451077043-hero1.jpg" alt="" className="img"  /></div>
           <div className="postfield">
           <h2 class="post-title-alt"></h2>
           <div className="text" dangerouslySetInnerHTML={{__html: x}} /> 
